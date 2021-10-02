3a5528e691869d8ceb632d391be59ac7c6ae1c22
Chithra Priya <chithrapriyaj@gmail.com>
2021-10-01 19:46:06 -0700

    add files

---
+++ MAIN/static/js/cpj_data.js
@@ -0,0 +1,30 @@
+
+let degreeData={
+    degrees_list :['Other', 'Associates Degree', 'Bachelors Degree', 'Doctorate', 'High School', 'Masters Degree', 'Vocational Certificate'],
+    degree_counts:[4830, 767, 1547, 25, 3023, 128, 168]
+};
+
+let regionData= {                
+regions_us: ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 
+  'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'USVIRGINISLANDS', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'],
+regions_count: [12, 141, 83, 296, 833, 115, 117, 43, 34, 762, 554, 20, 113, 45, 440, 267, 108, 149, 80, 267, 230, 32, 285, 235, 217, 119, 15, 399, 33, 54, 91, 312, 94, 81, 401, 441, 136, 122, 434, 9, 24, 168, 28, 325, 851, 2, 58, 235, 20, 293, 208, 42, 13]
+
+}; 
+
+let mastersData = {
+  Masters_counts: [18,17,12,8,7,6,6,5,5,5,5,4,4,3,3,3,2,2,2,2,2,1,1,1,1,1,1,1],
+  Masters_states:['CA', 'GA', 'FL', 'TX', 'MO', 'PA', 'NY', 'IL', 'VA', 'MA', 'NJ', 'MD', 'IN', 'TN', 'NC', 'OR', 'NH', 'MI', 'SC', 'CO', 'AL', 'WA', 'NM', 'DC', 'MN', 'ID', 'AZ', 'OH'],
+
+};
+                  
+let doctorateData = {
+    Doctorate_counts :[5, 4, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  Doctorates_states: ['CA', 'IL', 'OH', 'NJ', 'TX', 'IA', 'MD', 'MN', 'OK', 'PA', 'ID', 'MA', 'PR', 'LA', 'MI'], 
+};
+
+
+  
+
+
+
+
