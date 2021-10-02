acbc3bca3d50f0906fe520181c8911822c54f46b
FrankJiang1208 <frankjiang1208@gmail.com>
2021-10-02 11:48:09 -0700

    .

--- MAIN/static/js/data.js
+++ MAIN/static/js/data.js
@@ -1,5 +1,5 @@
 let major_data={
     major_sum:[11825.78, 957.82, 3750.24, 2071.48, 116.74, 4867.96, 327.44, 1298.38, 976.52, 5.0, 2945.18, 1352.84, 962.3, 550.68],
-    major:['Business', 'Computer and information sciences', 'Engineering', 'Communication journalism and related programs ', 'Transportation and materials moving', 'Education', 'Architecture and related services', 'Multi/interdisciplinary studies ', 'English language and literature/letters', 'Homeland security law enforcement and firefighting', 'Biological and biomedical sciences', 'Homeland security law enforcement and firefighting ', 'Legal professions and studies', 'Family and consumer sciences/human sciences'],
+    major:['Business', 'Computer sciences', 'Engineering', 'Communication', 'Transportation', 'Education', 'Architecture', 'Multidiscipline', 'English and literature', 'Homeland security', 'Bio sciences', 'law enforcement', 'Legal', 'consumer sciences'],
     job_titles:[8359, 2242, 2231, 1487, 1350, 1144, 621, 589, 355, 312, 272, 239, 200, 186]
 }
