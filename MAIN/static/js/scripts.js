0c366fed7ac27f8059f85aa257a878ac5418697f
FrankJiang1208 <frankjiang1208@gmail.com>
2021-10-03 11:45:03 -0700

    .

--- MAIN/static/js/scripts.js
+++ MAIN/static/js/scripts.js
@@ -1,35 +1,35 @@
-let trace1 = {
+let trace10 = {
     x: major_data.major,
     y: major_data.major_sum,
     type: "bar"
   };
-let trace2 = {
+let trace20 = {
     x: major_data.major,
     y: major_data.job_titles,
     type: "bar"
   };
 // Data trace array
-let traceData = [trace1,trace2];
+let traceData10 = [trace10,trace20];
 
 // Apply the group barmode to the layout
-let layout = {
+let layout10 = {
   title: "Job vs Major",
   barmode: 'group'
 };
 
 // Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", traceData, layout);
+Plotly.newPlot("plot", traceData10, layout10);
 
-let trace3={
+let trace30={
   x:comp_data.job_num,
   y: comp_data.major_num,
   type:'scatter'
 }
 
-let traceData1 = [trace3];
+let traceData20 = [trace30];
 
-let layout1 = {
+let layout20 = {
   title: "Job vs Major",
 };
 
-Plotly.newPlot("plot4", traceData1, layout1);
+Plotly.newPlot("plot4", traceData20, layout20);
