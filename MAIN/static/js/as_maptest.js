f6c72c2187a87d959aa91db6f89794386a048edf
FrankJiang1208 <frankjiang1208@gmail.com>
2021-10-03 12:16:09 -0700

    .

--- MAIN/static/js/as_maptest.js
+++ MAIN/static/js/as_maptest.js
@@ -1,15 +1,15 @@
-let myMap = L.map("map", {
-  center: [37.8, -96],
-  zoom: 5 ,
-  minZoom: 4,
-  // layers: [streets, geojson]
-});
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-  .addTo(myMap);
+// let myMap = L.map("map", {
+//   center: [37.8, -96],
+//   zoom: 5 ,
+//   minZoom: 4,
+//   // layers: [streets, geojson]
+// });
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   })
+//   .addTo(myMap);
 
-  geoData = "../static/resources/geojsons/newtest.geojson"
+geoData = "../static/resources/geojsons/newtest.geojson"
     
     
   let geojson;
@@ -19,32 +19,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   d3.json(geoData).then(function(data) {
 
     const bigboi = data.features
-    for (let i = 0; i < bigboi.length; i++) {
-        console.log(bigboi[i].properties.jobs['Job characteristic code'])
-    }
-      
+    console.log(bigboi);
+    keys=Object.keys(bigboi[0].properties.jobs['Job characteristic code'])
     dropdown=d3.select("#selDataset");
 
-    for (i=0;i<bigboi.length;i++) {
-      let op=dropdown.append("option").text(bigboi[i].properties.jobs['Job characteristic code'])
-      op.property('value',bigboi[i].properties.jobs['Job characteristic code'])
+    for (i=0;i<keys.length;i++) {
+      let op=dropdown.append("option").text(keys[i])
+      op.property('value',keys[i])
   }
+  let myMap = L.map("map", {
+    center: [37.8, -96],
+    zoom: 5 ,
+    minZoom: 4,
+    // layers: [streets, geojson]
+  });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    .addTo(myMap);
     d3.selectAll("#selDataset").on("change", getData);
 
 
     function getData(){
+      let v=dropdown.property('value');
+      console.log(v)
 
-
-      let myMap = L.map("map", {
-        center: [37.8, -96],
-        zoom: 5 ,
-        minZoom: 4,
-        // layers: [streets, geojson]
-      });
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        })
-        .addTo(myMap);
       
         geoData = "../static/resources/geojsons/newtest.geojson"
 
@@ -52,7 +51,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       geojson = L.choropleth(data, {
 
       // Define which property in the features to use.
-      valueProperty: bigboi[i].properties.jobs['Job characteristic code'],
+      valueProperty: v,
 
       // Set the color scale.
       scale: ["#5ba3ff", "#301934"],
@@ -78,33 +77,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     .addTo(myMap)
 
 
-    // Set up the legend.
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
+  //   // Set up the legend.
+  //   var legend = L.control({ position: "bottomright" });
+  //   legend.onAdd = function() {
+  //     var div = L.DomUtil.create("div", "info legend");
+  //     var limits = geojson.options.limits;
+  //     var colors = geojson.options.colors;
+  //     var labels = [];
 
-      // Add the minimum and maximum.
-      var legendInfo = "<h3>Occupation Density</h3>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
+  //     // Add the minimum and maximum.
+  //     var legendInfo = "<h3>Occupation Density</h3>" +
+  //       "<div class=\"labels\">" +
+  //         "<div class=\"min\">" + limits[0] + "</div>" +
+  //         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //       "</div>";
 
-      div.innerHTML = legendInfo;
+  //     div.innerHTML = legendInfo;
 
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
+  //     limits.forEach(function(limit, index) {
+  //       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //     });
 
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
+  //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //     return div;
+  //   };
 
-  //   Adding the legend to the map
-    legend.addTo(myMap);
+  // //   Adding the legend to the map
+  //   legend.addTo(myMap);
 
 
  }
