784b0bed06eb7b1a482a7723ac6b945b6d934d54
FrankJiang1208 <frankjiang1208@gmail.com>
2021-10-04 19:08:13 -0700

    .

--- MAIN/scrape.py
+++ MAIN/scrape.py
@@ -6,14 +6,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
 def scrape():
-    url="https://www.cnbc.com/jobs/"
+    url="https://money.cnn.com/news/specials/jobs/"
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
     browser.visit(url)
     html=browser.html
     soup = BeautifulSoup(html, 'html.parser')
-    result=soup.find_all('div',class_='Card-titleContainer')
-    img=soup.find('img',class_='Card-mediaContainerInner')['src']
-    latest=result[0].find('div').text
-    print(img)
-    return{'Title':latest,'Image':img}
+    result=soup.find_all('div',class_='cnnHeadline')
+    img=soup.find_all('a',class_='summaryImg')[0].find('img')['src']
+    latest=result[0].find('a').text
+    url=result[0].find('a')['href']
+    browser.quit()
+
+    return{'Title':latest,'Image':img,"Url":"https://money.cnn.com/"+url}
