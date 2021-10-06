851a9d1b1d79322b95535c8c7960d74ffb677571
FrankJiang1208 <frankjiang1208@gmail.com>
2021-10-06 16:35:39 -0700

    .

--- app.py
+++ app.py
@@ -20,11 +20,13 @@ def slides():
 
 @app.route("/job_market")
 def jm():
-    return render_template("job_market_map_mw.html")
+    return render_template("jobs_by_category_mw.html")
+    
 
 @app.route("/job_cat")
 def jc():
-    return render_template("jobs_by_category_mw.html")
+    return render_template("job_market_map_mw.html")
+    
 
 @app.route("/salary")
 def sa():
