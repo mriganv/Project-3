e566fd1593e9b0a7324656c44a89d434208620a6
FrankJiang1208 <frankjiang1208@gmail.com>
2021-11-23 15:47:05 -0800

    optimized machine learning model

--- static/model/app.py
+++ static/model/app.py
@@ -16,23 +16,44 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 import joblib
 
-col='Mean_Salary', 'Python', 'R', 'SQL', 'AWS', 'Excel', 'GCP', 'Azure', 'Spark', 'Tableau', 'Keras', 'NoSQL', 'Machine_Learning', 'Hadoop', 'AI', 'Power_BI', 'Job Title_data analyst', 'Job Title_data engineer', 'Job Title_data science related jobs', 'Job Title_data scientist', 'Job Title_director', 'Job Title_machine learning engineer', 'Job Title_manager', 'Category_Big Data Software Engineer', 'Category_Business Intelligence Analyst', 'Category_Business Intelligence Developer', 'Category_Business Systems Analyst', 'Category_Data Analyst', 'Category_Data Architect', 'Category_Data Engineer', 'Category_Data Modeler', 'Category_Data Scientist', 'Category_Data Storyteller', 'Category_Data Warehouse Manager', 'Category_Database Administrator', 'Category_Database Manager', 'Category_IT Systems Analyst', 'Category_Logistics Analyst', 'Category_Machine Learning Engineer', 'Category_Machine Learning Scientist', 'Category_Marketing Analyst', 'Category_Quantitative Analyst', 'Category_Statistician', 'state_ AK', 'state_ AL', 'state_ AR', 'state_ AZ', 'state_ CA', 'state_ CO', 'state_ CT', 'state_ DC', 'state_ DE', 'state_ FL', 'state_ GA', 'state_ HI', 'state_ IA', 'state_ ID', 'state_ IL', 'state_ IN', 'state_ KS', 'state_ KY', 'state_ LA', 'state_ MA', 'state_ MD', 'state_ ME', 'state_ MI', 'state_ MN', 'state_ MO', 'state_ MS', 'state_ MT', 'state_ NC', 'state_ NE', 'state_ NH', 'state_ NJ', 'state_ NM', 'state_ NV', 'state_ NY', 'state_ OH', 'state_ OK', 'state_ OR', 'state_ PA', 'state_ RI', 'state_ SC', 'state_ TN', 'state_ TX', 'state_ UT', 'state_ VA', 'state_ VI', 'state_ VT', 'state_ WA', 'state_ WI', 'state_ WV', 'state_ WY', 'Seniority_Not Specified', 'Seniority_Senior'
+
+class TextSelector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    Use on text columns in the data
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.key]
+    
+class NumberSelector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    Use on numeric columns in the data
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[[self.key]]
 
 def salary_category(bin):
 
     if bin==1:
-        return "Job salary is estimated be between $50000 and $75000"
+        return "Job salary is estimated be between $50000 and $100000"
     if bin==2:
-        return "Job salary is estimated be between $75000 and $100000"
+        return "Job salary is estimated be between $100000 and $150000"
     if bin==3:
-        return "Job salary is estimated be between $100000 and $125000"
+        return "Job salary is estimated be between $150000 and $200000"
     if bin==4:
-        return "Job salary is estimated be between $125000 and $150000"
-    if bin==5:
-        return "Job salary is estimated be between $150000 and $175000"
-    if bin==6:
-        return "Job salary is estimated be between $175000 and $200000"
-    if bin==7:
         return "Job salary is estimated be more than $200000"
 
 def skill_check(dict):
